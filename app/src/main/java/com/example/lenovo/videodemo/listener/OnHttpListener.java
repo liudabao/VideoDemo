d60ea588b1c731a1f86d5c352c1ea3674eb238d1
liudabao <15810163587@163.com>
2016-07-17 23:16:39 +0800

    improve

--- app/src/main/java/com/example/lenovo/videodemo/listener/OnHttpListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.videodemo.listener;
-
-/**
- * Created by lenovo on 2016/7/12.
- */
-public interface OnHttpListener {
-    public void onSuccess(String response);
-    public void onFailed();
-}
--- app/src/main/java/com/example/lenovo/videodemo/listener/OnProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.videodemo.listener;
-
-/**
- * Created by lenovo on 2016/7/15.
- */
-public interface OnProgressListener {
-
-    public void onProgress(int percent);
-}
